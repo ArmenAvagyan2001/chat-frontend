bf4e86c2878994155b8e82cae6049484325044a7
Armen2022 <avakanarmen226@gmail.com>
2023-02-20 12:33:15 +0400

    fix issues

--- src/http/index.js
+++ src/http/index.js
@@ -18,13 +18,13 @@ $api.interceptors.response.use((config) => {
     const originalRequest = error.config;
     if (error.response.status === 401 && error.config && !error.config._isRetry && window.localStorage.getItem('token')) {
         originalRequest._isRetry = true
-        await axios.get(`${API_URL}/api/refresh`)
+        await $api.get(`${API_URL}/api/refresh`)
             .then(res => {
                 window.localStorage.setItem("token", res.data.accessToken)
             }).catch(async ({response}) => {
                 console.log(response)
                 window.localStorage.clear()
-                // window.location.reload()
+                window.location.reload()
             })
 
         return $api.request(originalRequest);
